bc5bb9272feaab790e8eab062adcd269c14a75b9
Eric Cristo <ericcristo2@gmail.com>
2023-06-23 16:27:47 +0100

    Aprendendo sobre Objetos

---
+++ o_que_sao_objetos/objeto.js
@@ -0,0 +1,13 @@
+const listaCPFs = [11111111, 22222, 33333]
+
+const informacoesPessoas = ["nome", "jose", "idade", 32, "CPF", "11122233344"];
+
+//console.log(informacoesPessoas[1]);
+
+const objetoPessoa = {
+    nome: "Jose",
+    idade: 32,
+    CPF: "11122233344"
+}
+
+console.log(objetoPessoa);
